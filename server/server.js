9bfa46dd52eab2e5460f9e75325eb9c4408da6a9
Paolo <fedeli.paolol@gmail.com.com>
2020-04-26 16:46:18 +0200

    useful commit message

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const socketIo = require("socket.io");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT
 
 const app = express();
 
