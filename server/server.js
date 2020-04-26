3e194ad141776c65b922f292d95566adf24dee08
Paolo <fedeli.paolol@gmail.com.com>
2020-04-26 16:21:46 +0200

    useful commit message

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 
 app.use("/api", require("./routes/card.routes"));
 
