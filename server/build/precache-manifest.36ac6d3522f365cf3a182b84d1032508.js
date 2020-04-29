a0ce7b757928cdf95730704f791b13ef74be3320
Paolo <fedeli.paolol@gmail.com.com>
2020-04-29 16:54:47 +0200

    useful commit message

---
+++ server/build/precache-manifest.36ac6d3522f365cf3a182b84d1032508.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b659850eecebc51d0ffdd295a4a8b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4bc8e3e503e66fba1ef",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a58fa51d5fa6455e1b3f",
+    "url": "/static/css/main.e1cd00d8.chunk.css"
+  },
+  {
+    "revision": "b4bc8e3e503e66fba1ef",
+    "url": "/static/js/2.22c1336c.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.22c1336c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58fa51d5fa6455e1b3f",
+    "url": "/static/js/main.168fb78d.chunk.js"
+  },
+  {
+    "revision": "e85bb92cb75e2c10a587",
+    "url": "/static/js/runtime-main.aae5a3be.js"
+  },
+  {
+    "revision": "4764c18d7100f776eedfb357bb03ce05",
+    "url": "/static/media/WelcomeToCardBis.4764c18d.jpg"
+  },
+  {
+    "revision": "d328a80bf4ff8aeece6ede8d4dc658f9",
+    "url": "/static/media/WelcomeToCardEmptyBack.d328a80b.jpg"
+  },
+  {
+    "revision": "bb33b3bc89d7b4bbe22842726c9d3546",
+    "url": "/static/media/WelcomeToCardFences.bb33b3bc.jpg"
+  },
+  {
+    "revision": "2b0ea696636d715d0999ed3e1ffa6fd9",
+    "url": "/static/media/WelcomeToCardMarket.2b0ea696.jpg"
+  },
+  {
+    "revision": "8afcda1127f03b0c3d1f7e01a81dc37a",
+    "url": "/static/media/WelcomeToCardPanorama.8afcda11.jpg"
+  },
+  {
+    "revision": "0c5913587f51ae9786c140339089f21e",
+    "url": "/static/media/WelcomeToCardSampleBack.0c591358.jpg"
+  },
+  {
+    "revision": "e714c0a9b1d830448e9c4d89abca0018",
+    "url": "/static/media/WelcomeToCardSwimpoll.e714c0a9.jpg"
+  },
+  {
+    "revision": "66943acff1b91ef5a3d9ecfb6dda71b5",
+    "url": "/static/media/WelcomeToCardWork.66943acf.jpg"
+  },
+  {
+    "revision": "d933d6380b519aa3d5be8300507eb662",
+    "url": "/static/media/Welcome_To_Fillable_Sheet_PDF_v2.0.d933d638.pdf"
+  },
+  {
+    "revision": "4229984cb12a5313d4d7d5e12f754b72",
+    "url": "/static/media/board.4229984c.png"
+  },
+  {
+    "revision": "ae53fe0f02dd809a4e34a578d72a6189",
+    "url": "/static/media/card1-1.ae53fe0f.jpg"
+  },
+  {
+    "revision": "8a629a64750568449da613ca2b5faeee",
+    "url": "/static/media/card1-2.8a629a64.jpg"
+  },
+  {
+    "revision": "585227c02e15ba6cf2bdbd01fe0b6849",
+    "url": "/static/media/card1-3.585227c0.jpg"
+  },
+  {
+    "revision": "ceeb5acc936aa11bf818c9b7b5360662",
+    "url": "/static/media/card1-4.ceeb5acc.jpg"
+  },
+  {
+    "revision": "47cd86bffa741f5aa1985f7b39e53c8d",
+    "url": "/static/media/card1-5.47cd86bf.jpg"
+  },
+  {
+    "revision": "28b5e5011248250e5b0360bd50aebb2a",
+    "url": "/static/media/card1-6.28b5e501.jpg"
+  },
+  {
+    "revision": "cc014c7ccea7f93f3df35b1860d247e8",
+    "url": "/static/media/card2-1.cc014c7c.jpg"
+  },
+  {
+    "revision": "c2ca6aec4f675b1b01c609da4289acc0",
+    "url": "/static/media/card2-2.c2ca6aec.jpg"
+  },
+  {
+    "revision": "8c656efbd979c2151c58062a19f2ccbd",
+    "url": "/static/media/card2-3.8c656efb.jpg"
+  },
+  {
+    "revision": "545a1bed546799eb67cccf95c78e7bd1",
+    "url": "/static/media/card2-4.545a1bed.jpg"
+  },
+  {
+    "revision": "23b56c3afac7cdf93d36c6579f461480",
+    "url": "/static/media/card2-5.23b56c3a.jpg"
+  },
+  {
+    "revision": "61164fcb1f71c6618468a0e3386ae0f6",
+    "url": "/static/media/card2-6.61164fcb.jpg"
+  },
+  {
+    "revision": "da7e8bafeb21439844483a65ea721700",
+    "url": "/static/media/card3-1.da7e8baf.jpg"
+  },
+  {
+    "revision": "1a0c8cd10577e313736f303520ffeb1d",
+    "url": "/static/media/card3-2.1a0c8cd1.jpg"
+  },
+  {
+    "revision": "83612a8be932138ee778f5eda7ff0ec5",
+    "url": "/static/media/card3-3.83612a8b.jpg"
+  },
+  {
+    "revision": "c15afd2b632f22dd019f805585582330",
+    "url": "/static/media/card3-4.c15afd2b.jpg"
+  },
+  {
+    "revision": "29c6d419655c84af0558ca17eb1850db",
+    "url": "/static/media/card3-5.29c6d419.jpg"
+  },
+  {
+    "revision": "d237739cafb59008705f92afdf9e91eb",
+    "url": "/static/media/card3-6.d237739c.jpg"
+  },
+  {
+    "revision": "fa5cf0f7944a6364196b52c78c9bda37",
+    "url": "/static/media/pic3761012.fa5cf0f7.jpg"
+  },
+  {
+    "revision": "6b2c0369438c54c4757f7f255a515b60",
+    "url": "/static/media/welcometogamebackground.6b2c0369.jpg"
+  },
+  {
+    "revision": "70480d1d67b6d1751c340998c3209838",
+    "url": "/static/media/welcometoslider.70480d1d.jpg"
+  }
+]);
