6af5eb9e29eafb5b0592667c406a090e800128a7
Paolo <fedeli.paolol@gmail.com.com>
2020-05-13 16:41:32 +0200

    useful commit message

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use("/api", require("./routes/card.routes"));
 
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 let server = app.listen(port);
