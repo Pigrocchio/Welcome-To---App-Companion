268f8006cba517a99c7659793007fba49ec689d7
Paolo <fedeli.paolol@gmail.com.com>
2020-05-13 08:57:26 +0200

    whit gameId session

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
 app.use("/api", require("./routes/card.routes"));
 
 let server = app.listen(port);
